549b0f8409f2bf8980313f49509b749a145c0f6b
Sadik <anarul6777@gmail.com>
2023-05-03 07:55:02 +0600

    banner is done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,36 @@
-import React, { useContext } from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../../providers/AuthProvider';
+// import React, { useContext } from 'react';
+// import { Container, Nav, Navbar } from 'react-bootstrap';
+// import { Link } from 'react-router-dom';
+// import { AuthContext } from '../../providers/AuthProvider';
 
-const Header = () => {
-    const {user, logOUt} = useContext(AuthContext);
-    console.log(user);
-    const handelLouOut = () =>{
-        logOUt()
-        .then(result =>{})
-        .catch(e => {
-            console.log(e.message);
-        })
-    }
-    return (
-        <div>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="#home">The Chef's Palette</Navbar.Brand>
-                    <Nav className="">
-                        <Link to="/">Home</Link>
-                        <Link className='mx-2' to="/blogs">BLogs</Link>
-                        <Link className='mx-2' to="/register">Register</Link>
-                        {user?<Link className='mx-2' onClick={handelLouOut} to="/">Logout</Link>:
-                        <Link to="/login">Login</Link>}
-                        {!!user && <Link to="/user">{user.email}</Link>}
-                    </Nav>
-                </Container>
-            </Navbar>
+// const Header = () => {
+//     const {user, logOUt} = useContext(AuthContext);
+//     console.log(user);
+//     const handelLouOut = () =>{
+//         logOUt()
+//         .then(result =>{})
+//         .catch(e => {
+//             console.log(e.message);
+//         })
+//     }
+//     return (
+//         <div>
+//             <Navbar bg="dark" variant="dark">
+//                 <Container>
+//                     <Navbar.Brand href="#home">The Chef's Palette</Navbar.Brand>
+//                     <Nav className="">
+//                         <Link to="/">Home</Link>
+//                         <Link className='mx-2' to="/blogs">BLogs</Link>
+//                         <Link className='mx-2' to="/register">Register</Link>
+//                         {user?<Link className='mx-2' onClick={handelLouOut} to="/">Logout</Link>:
+//                         <Link to="/login">Login</Link>}
+//                         {!!user && <Link to="/user">{user.email}</Link>}
+//                     </Nav>
+//                 </Container>
+//             </Navbar>
 
-        </div>
-    );
-};
+//         </div>
+//     );
+// };
 
-export default Header;
+// export default Header;
