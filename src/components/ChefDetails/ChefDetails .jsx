79292025b50a4ea6febfecc89cb2c34ec8320e1e
Sadik <anarul6777@gmail.com>
2023-05-03 23:39:33 +0600

    recipe card is done

--- src/components/ChefDetails/ChefDetails .jsx
+++ src/components/ChefDetails/ChefDetails .jsx
@@ -1,6 +1,7 @@
 
 import { useLoaderData } from 'react-router-dom';
 import './ChefDetails.css'
+import { Button, Card } from 'react-bootstrap';
 
 const ChefDetails = () => {
 
@@ -9,21 +10,33 @@ const ChefDetails = () => {
 
   return (
     <div className='container'>
-      <div className='my-class' style={{display: 'flex', margin: '20px 0', border: '2px solid', borderRadius: '20px', padding: '10px'}}>
+      <div className='my-class' style={{ display: 'flex', margin: '20px 0', border: '2px solid', borderRadius: '20px', padding: '10px' }}>
         <div className='me-5'>
-          <img className='chef_img' style={{borderRadius: '10px', width: '340px'}} src={chef_img} alt="" />
+          <img className='chef_img' style={{ borderRadius: '10px', width: '340px' }} src={chef_img} alt="" />
         </div>
         <div>
           <h4><span className='fw-bold'>Name</span>: {chef_name}</h4>
           <p><span className='fw-bold'>Experience</span>: {years_of_experience} Years</p>
           <p>{short_bio}</p>
           <p>{description}</p>
-          <div style={{display: 'flex'}}>
+          <div style={{ display: 'flex' }}>
             <p className='me-5'><span className='fw-bold'>Likes</span>: {likes}</p>
             <p><span className='fw-bold'>Recipe Number</span>: {number_of_recipes}</p>
           </div>
         </div>
       </div>
+
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={recipes_img} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
     </div>
   );
 };
